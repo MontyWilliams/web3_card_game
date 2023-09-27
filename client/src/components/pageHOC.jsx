a48414f3012402f924d6490c9613d40e8fe14657
Montywilliams <3650@holbertonschool.com>
2023-09-27 12:19:59 -0600

    import styles; this is a higher order component

---
+++ client/src/components/pageHOC.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import { logo, heroImg } from '../assets'
+import styles from '../styles'
+
+function pageHOC() {
+  return (
+    <div>pageHOC</div>
+  )
+}
+
+export default pageHOC
