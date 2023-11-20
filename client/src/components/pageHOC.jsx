05f7135943c9696e6d4fa40d47f0ca2de2af0c80
Montywilliams <3650@holbertonschool.com>
2023-11-20 10:42:48 -0600

    correct typo

--- client/src/components/pageHOC.jsx
+++ client/src/components/pageHOC.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 import { logo, heroImg } from '../assets'
 import styles from '../styles'
 
 const pageHOC = (Component, title, descritpion) => () => {
-  const navigat = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className={styles.hocContainer}>
       <div className={styles.hocContentBox}>
-
+        <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')}/>
       </div>
     </div>
   )
